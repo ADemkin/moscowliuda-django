6fbc31dea18447c3835b8468a91ffce0fd834e37
fedorkuruts <fedorkuruts@gmail.com>
2024-03-02 14:37:19 +0600

    Рефаксторинг согласно запросу

--- add_items_to_db.py
+++ add_items_to_db.py
@@ -4,53 +4,56 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moscowliuda.settings')
 import django
 
 django.setup()
-from goods.models import Photo, Url, TextBook, Project
+from goods.models import Photo, Url, Good, Project
 
-photo1 = Photo.objects.create(name='Фото 1', photo='photos/photo1.jpg')
-photo2 = Photo.objects.create(name='Фото 2', photo='photos/photo2.jpg')
-photo3 = Photo.objects.create(name='Фото 3', photo='photos/photo3.jpg')
-photo4 = Photo.objects.create(name='Фото 4', photo='photos/photo4.jpg')
 
-url1 = Url.objects.create(name='Ссылка 1', url='https://www.youtube.com/watch?v=BSlfcu4nDQI')
-url2 = Url.objects.create(name='Ссылка 2', url='https://www.youtube.com/watch?v=a7jYN_TToCY')
-url3 = Url.objects.create(name='Ссылка 3', url='https://www.youtube.com/watch?v=CP14arpa84M')
+def create_database_test_values():
+    try:
+        good1 = Good.objects.create(
+            title='Good 1',
+            sub_title='Sub Title 1',
+            price_rub=500,
+            main_photo='photos/photo1.jpg',
+            description='Description for Good 1'
+        )
 
-textbook1 = TextBook.objects.create(
-    title='Учебник 1',
-    sub_title='Подзаголовок 1',
-    price_rub=500,
-    main_photo=photo1,
-    description='Описание учебника 1'
-)
-textbook1.secondary_photos.add(photo2, photo3)
-textbook1.urls.add(url1, url2)
+        good2 = Good.objects.create(
+            title='Good 2',
+            sub_title='Sub Title 2',
+            price_rub=600,
+            main_photo='photos/photo2.jpg',
+            description='<b>Жырный текст</b><br>Обычный текст<p>Параграф</p>'
+        )
 
-textbook2 = TextBook.objects.create(
-    title='Учебник 2',
-    sub_title='Подзаголовок 2',
-    price_rub=600,
-    main_photo=photo2,
-    description='Описание учебника 2'
-)
-textbook2.secondary_photos.add(photo1, photo4)
-textbook2.urls.add(url2, url3)
+        project1 = Project.objects.create(
+            title='Project 1',
+            sub_title='Sub Title 1',
+            main_photo='photos/photo2.jpg',
+            description='Description for Project 1'
+        )
 
-project1 = Project.objects.create(
-    title='Проект 1',
-    sub_title='Подзаголовок 1',
-    main_photo=photo3,
-    description='Описание проекта 1'
-)
-project1.secondary_photos.add(photo1, photo2)
-project1.urls.add(url1, url3)
+        project2 = Project.objects.create(
+            title='Project 2',
+            sub_title='Sub Title 2',
+            main_photo='photos/photo1.jpg',
+            description='Description for Project 2'
+        )
 
-project2 = Project.objects.create(
-    title='Проект 2',
-    sub_title='Подзаголовок 2',
-    main_photo=photo4,
-    description='Описание проекта 2'
-)
-project2.secondary_photos.add(photo2, photo3)
-project2.urls.add(url2)
+        # photo1 = Photo.objects.create(name='Photo 1', photo='photos/photo1.jpg', good=good1, project=project1)
+        photo2 = Photo.objects.create(name='Photo 2', photo='photos/photo2.jpg', good=good1, project=project1)
+        photo3 = Photo.objects.create(name='Photo 3', photo='photos/photo3.jpg', good=good2, project=project2)
+        photo4 = Photo.objects.create(name='Photo 4', photo='photos/photo4.jpg', good=good2, project=project2)
 
-print('База данных успешно заполнена!')
+        url1 = Url.objects.create(name='Url 1', url='https://www.youtube.com/watch?v=1', good=good1, project=project1)
+        url2 = Url.objects.create(name='Url 2', url='https://www.youtube.com/watch?v=2', good=good1, project=project1)
+        url3 = Url.objects.create(name='Url 3', url='https://www.youtube.com/watch?v=3', good=good2, project=project2)
+        url4 = Url.objects.create(name='Url 4', url='https://www.youtube.com/watch?v=4', good=good2, project=project2)
+
+        print('Testing set in DB created successfully')
+    except Exception as e:
+        print(f'[ERROR] Cannot to create test dataset in db: {e}')
+        return
+
+
+if __name__ == '__main__':
+    create_database_test_values()
--- goods/models.py
+++ goods/models.py
@@ -1,9 +1,41 @@
 from django.db import models
 
 
+class Good(models.Model):
+    title = models.CharField(verbose_name='Заголовок', max_length=100)
+    sub_title = models.CharField(verbose_name='Подзаголовок', max_length=100)
+    price_rub = models.IntegerField(verbose_name='Цена')
+    main_photo = models.ImageField(null=True, upload_to='photos/', verbose_name='Главное фото')
+    description = models.TextField(verbose_name='Описание', max_length=4000)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+
+    class Meta:
+        verbose_name = 'Пособие'
+        verbose_name_plural = 'Пособия'
+
+    def __str__(self):
+        return self.title
+
+
+class Project(models.Model):
+    title = models.CharField(verbose_name='Заголовок', max_length=100)
+    sub_title = models.CharField(verbose_name='Подзаголовок', max_length=100)
+    main_photo = models.ImageField(null=True, upload_to='photos/', verbose_name='Главное фото')
+    description = models.TextField(verbose_name='Описание', max_length=4000)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+
+    class Meta:
+        verbose_name = 'Проект'
+        verbose_name_plural = 'Проекты'
+
+
 class Photo(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название фото')
     photo = models.ImageField(upload_to='photos/', verbose_name='Фото')
+    good = models.ForeignKey(Good, null=True,blank=True, on_delete=models.CASCADE, verbose_name='Пособие',
+                             related_name='good_photos')
+    project = models.ForeignKey(Project, null=True,blank=True, on_delete=models.CASCADE, verbose_name='Проект',
+                                related_name='project_photos')
 
     class Meta:
         verbose_name = 'Фото'
@@ -16,6 +48,10 @@ class Photo(models.Model):
 class Url(models.Model):
     name = models.CharField(max_length=100)
     url = models.URLField()
+    good = models.ForeignKey(Good, null=True,blank=True, on_delete=models.CASCADE, verbose_name='Пособие',
+                             related_name='good_urls')
+    project = models.ForeignKey(Project, null=True, blank=True, on_delete=models.CASCADE, verbose_name='Проект',
+                                related_name='project_urls')
 
     def __str__(self):
         return f"{self.id} - {self.name}"
@@ -23,44 +59,3 @@ class Url(models.Model):
     class Meta:
         verbose_name = 'Ссылка на ютуб'
         verbose_name_plural = 'Ссылки на ютуб'
-
-
-class Good(models.Model):
-    title = models.CharField(verbose_name='Заголовок', max_length=100)
-    sub_title = models.CharField(verbose_name='Подзаголовок', max_length=100)
-    price_rub = models.IntegerField(verbose_name='Цена')
-    main_photo = models.ImageField(null=True, verbose_name='Главное фото', related_name='main_photo_of')
-    urls = models.ManyToManyField(Url, verbose_name='Ссылки на ютуб', related_name='text_books', blank=True)
-    description = models.TextField(verbose_name='Описание', max_length=4000)
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
-
-    class Meta:
-        verbose_name = 'Учебник'
-        verbose_name_plural = 'Учебники'
-
-    def __str__(self):
-        return self.title
-
-
-class Project(models.Model):
-    title = models.CharField(verbose_name='Заголовок', max_length=100)
-    sub_title = models.CharField(verbose_name='Подзаголовок', max_length=100)
-    main_photo = models.OneToOneField(
-        Photo,
-        on_delete=models.SET_NULL,
-        null=True,
-        verbose_name='Главное фото',
-        related_name='main_photo_of_project'
-    )
-
-    description = models.TextField(verbose_name='Описание', max_length=4000)
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
-    urls = models.ManyToManyField(
-        Url,
-        verbose_name='Ссылки на ютуб',
-        related_name='projects',
-        blank=True
-    )
-    class Meta:
-        verbose_name = 'Проект'
-        verbose_name_plural = 'Проекты'
